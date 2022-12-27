4db116a1b10dc63b8b1de6a124b5bad34eddcba8
Owinnie <tillawinnie@gmail.com>
2022-12-27 11:33:53 +0300

    then() catch() finally()

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,11 +1,7 @@
 export default function handleResponseFromAPI(promise) {
-  return new Promise((resolve, reject) => {
-    console.log('Got a response from the API');
-    if (promise) {
-      resolve({ status: 200, body: 'success' });
-    }
-    else if (!promise) {
-      reject(new Error());
-    }
-  });
+  const res = { status: 200, body: 'success' };
+  return promise
+    .then(() => res)
+    .catch(() => new Error())
+    .finally(() => { console.log('Got a response from the API'); });
 }
