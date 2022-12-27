1d5bbc1b0e8dcd3ac52d5d88100040e4deb77897
Owinnie <tillawinnie@gmail.com>
2022-12-27 10:27:27 +0300

    Lint errors

--- 0x01-ES6_promise/1-promise.js
+++ 0x01-ES6_promise/1-promise.js
@@ -1,9 +1,9 @@
 export default function getFullResponseFromAPI(success) {
   return new Promise((resolve, reject) => {
-    if (success == true) {
-      resolve({'status': 200, 'body': 'Success'});
+    if (success === true) {
+      resolve({ status: 200, body: 'Success' });
     }
-    else if(success == false) {
+    else if(success === false) {
       reject(Error('The fake API is not working currently'));
     }
   });
