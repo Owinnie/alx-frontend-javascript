ce848fc982deb529de99c340a2dec413f16e962e
Owinnie <tillawinnie@gmail.com>
2023-01-05 19:32:15 +0300

    a simple promise

--- 0x01-ES6_promise/4-user-promise.js
+++ 0x01-ES6_promise/4-user-promise.js
@@ -1,6 +1,6 @@
 export default function signUpUser(firstName, lastName) {
   return new Promise((resolve, reject) => {
-    resolve({ firstName: firstName, lastName: lastName });
+    resolve({ firstName, lastName });
     reject(new Error());
   });
 }
