846d7010f4c58802fd9890efe275ed8ad1736e3d
Owinnie <tillawinnie@gmail.com>
2023-01-05 17:43:13 +0300

    return promise from a function

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -2,7 +2,7 @@ export default function getResponseFromAPI() {
   return new Promise((resolve, reject) => {
     setTimeout(() => {
       resolve('Intro to promises');
-      reject('I have no use at this time');
+      reject(new Error('I have no use at this time'));
     }, 300);
   });
 }
