9e9bc831342002f5abb65351df8f6aeaa7d3de47
Owinnie <tillawinnie@gmail.com>
2023-01-05 17:58:06 +0300

    promises

--- 0x01-ES6_promise/1-promise.js
+++ 0x01-ES6_promise/1-promise.js
@@ -2,9 +2,8 @@ export default function getFullResponseFromAPI(success) {
   return new Promise((resolve, reject) => {
     if (success === true) {
       resolve({ status: 200, body: 'Success' });
-    }
-    else if(success === false) {
-      reject(Error('The fake API is not working currently'));
+    } else {
+      reject(new Error('The fake API is not working currently'));
     }
   });
 }
