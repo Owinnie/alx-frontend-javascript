981de5648e13604bdcd662cd156a7312b0f0c65d
Owinnie <tillawinnie@gmail.com>
2023-01-05 10:03:01 +0300

    Pricing class

---
+++ 0x02-ES6_classes/4-pricing.js
@@ -0,0 +1,38 @@
+import Currency from './3-currency.js';
+
+export default class Pricing {
+  constructor(amount, currency) {
+    this.amount = amount;
+    this.currency = currency;
+  }
+
+  // Getter: amount
+  get amount() {
+    return this._amount;
+  }
+
+  // Setter: amount
+  set amount(value) {
+    this._amount = value;
+  }
+
+  // Getter: currency
+  get currency() {
+    return this._currency;
+  }
+
+  // Setter: currency
+  set currency(value) {
+    this._currency = value;
+  }
+
+  // class methods:
+  displayFullPrice() {
+    return `${this.amount} ${this.currency.name} (${this.currency.code})`;
+  }
+
+  // static methods
+  static convertPrice(amount, conversionRate) {
+    return amount * conversionRate;
+  }
+}
