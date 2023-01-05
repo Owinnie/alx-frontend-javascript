62b80d06622da9058519f294e1648f0e6742e9b9
Owinnie <tillawinnie@gmail.com>
2023-01-05 18:49:42 +0300

    promises all

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -2,14 +2,18 @@
 import { uploadPhoto, createUser } from './utils.js';
 
 export default function handleProfileSignup() {
+  let fName;
+  let lName;
+  let body;
+
   const photo = uploadPhoto();
   const user = createUser();
 
   return Promise.all([photo, user])
     .then((resp) => {
-      const body = resp[0].body;
-      const fName = resp[1].firstName;
-      const lName = resp[1].lastName;
+      body = resp[0].body;
+      fName = resp[1].firstName;
+      lName = resp[1].lastName;
       console.log(`${body} ${fName} ${lName}`);
     })
     .catch(() => { console.log('Signup system offline'); });
