c3071f8586bc49cc270205e9d6da0908ecd46235
Owinnie <tillawinnie@gmail.com>
2023-01-05 09:15:02 +0300

    HolbertonCourse class

---
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -0,0 +1,48 @@
+export default class HolbertonCourse {
+  constructor(name, length, students) {
+    this.name = name;
+    this.length = length;
+    this.students = students;
+  }
+
+  // name getter
+  get name() {
+    return this._name;
+  }
+
+  // name setter
+  set name(value) {
+    if (typeof value !== 'string') {
+      throw new TypeError('Name must be of type string');
+    }
+    this._name = value;
+  }
+
+  // length getter
+  get length() {
+    return this._length;
+  }
+
+  // length setter
+  set length(value) {
+    if (typeof value !== 'number') {
+      throw new TypeError('Length must be of type number');
+    }
+    this._length = value;
+  }
+
+  // students getter
+  get students() {
+    return this._students;
+  }
+
+  // students setter
+  set students(value) {
+    for (const v of value) {
+      if (typeof v !== 'string') {
+        throw new TypeError('Students must be an array of strings');
+      }
+    }
+    this._students = value;
+  }
+}
