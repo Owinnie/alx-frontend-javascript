f7011777e56508fd6550e1e9b98b13a659661e1f
Owinnie <tillawinnie@gmail.com>
2023-01-05 17:34:44 +0300

    return promise from a function

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -1,3 +1,7 @@
 export default function getResponseFromAPI() {
-  return new Promise((resolve, reject) => [resolve, reject]);
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve("Owinnie");
+    }, 300);
+  });
 }
